b6e7a3b61adf7adf83c7d78fca9280ddb5c710ec
Dan Fisher <dan.fisher85@yahoo.com>
2025-10-01 19:58:41 +0200

    updates options position

--- includes/class-mtp-admin-meta-boxes.php
+++ includes/class-mtp-admin-meta-boxes.php
@@ -153,6 +153,13 @@ class MTP_Admin_Meta_Boxes {
     // Dimensions
     $this->render_number_field('width', __('Table Width (px)', 'meinturnierplan-wp'), $meta_values['width'], __('Set the width of the tournament table in pixels.', 'meinturnierplan-wp'), 100, 2000);
     $this->render_number_field('height', __('Table Height (px)', 'meinturnierplan-wp'), $meta_values['height'], __('Set the height of the tournament table in pixels.', 'meinturnierplan-wp'), 100, 2000);
+
+    // Display options
+    $this->render_checkbox_field('suppress_wins', __('Suppress Num Wins, Losses, etc.', 'meinturnierplan-wp'), $meta_values['suppress_wins'], __('Hide the number of wins, losses, and other statistical columns from the tournament table.', 'meinturnierplan-wp'));
+    $this->render_checkbox_field('suppress_logos', __('Suppress Logos', 'meinturnierplan-wp'), $meta_values['suppress_logos'], __('Hide the logos from the tournament table.', 'meinturnierplan-wp'));
+    $this->render_checkbox_field('suppress_num_matches', __('Suppress Num Matches', 'meinturnierplan-wp'), $meta_values['suppress_num_matches'], __('Hide the number of matches from the tournament table.', 'meinturnierplan-wp'));
+    $this->render_checkbox_field('projector_presentation', __('Projector Presentation', 'meinturnierplan-wp'), $meta_values['projector_presentation'], __('Enable projector presentation mode for the tournament table.', 'meinturnierplan-wp'));
+    $this->render_checkbox_field('navigation_for_groups', __('Navigation for Groups', 'meinturnierplan-wp'), $meta_values['navigation_for_groups'], __('Enable navigation for groups in the tournament table.', 'meinturnierplan-wp'));
     
     // Font sizes
     $this->render_number_field('font_size', __('Content Font Size (pt)', 'meinturnierplan-wp'), $meta_values['font_size'], __('Set the font size of the tournament table content. 9pt is the default value.', 'meinturnierplan-wp'), 6, 24);
@@ -188,13 +195,6 @@ class MTP_Admin_Meta_Boxes {
     $this->render_color_opacity_field('odd_bg_color', 'odd_bg_opacity', __('Odd Rows Background Color', 'meinturnierplan-wp'), $meta_values['odd_bg_color'], $meta_values['odd_bg_opacity'], __('Set the background color and opacity for odd-numbered table rows. Use opacity 0% for transparent background.', 'meinturnierplan-wp'));
     $this->render_color_opacity_field('hover_bg_color', 'hover_bg_opacity', __('Row Hover Background Color', 'meinturnierplan-wp'), $meta_values['hover_bg_color'], $meta_values['hover_bg_opacity'], __('Set the background color and opacity for table rows hover. Use opacity 0% for transparent background.', 'meinturnierplan-wp'));
     $this->render_color_opacity_field('head_bg_color', 'head_bg_opacity', __('Head Background Color', 'meinturnierplan-wp'), $meta_values['head_bg_color'], $meta_values['head_bg_opacity'], __('Set the background color and opacity for table head. Use opacity 0% for transparent background.', 'meinturnierplan-wp'));
-    
-    // Display options
-    $this->render_checkbox_field('suppress_wins', __('Suppress Num Wins, Losses, etc.', 'meinturnierplan-wp'), $meta_values['suppress_wins'], __('Hide the number of wins, losses, and other statistical columns from the tournament table.', 'meinturnierplan-wp'));
-    $this->render_checkbox_field('suppress_logos', __('Suppress Logos', 'meinturnierplan-wp'), $meta_values['suppress_logos'], __('Hide the logos from the tournament table.', 'meinturnierplan-wp'));
-    $this->render_checkbox_field('suppress_num_matches', __('Suppress Num Matches', 'meinturnierplan-wp'), $meta_values['suppress_num_matches'], __('Hide the number of matches from the tournament table.', 'meinturnierplan-wp'));
-    $this->render_checkbox_field('projector_presentation', __('Projector Presentation', 'meinturnierplan-wp'), $meta_values['projector_presentation'], __('Enable projector presentation mode for the tournament table.', 'meinturnierplan-wp'));
-    $this->render_checkbox_field('navigation_for_groups', __('Navigation for Groups', 'meinturnierplan-wp'), $meta_values['navigation_for_groups'], __('Enable navigation for groups in the tournament table.', 'meinturnierplan-wp'));
     
     echo '</table>';
   }
